4a79a50d99852ccfb02937bf3c585bbcf6711c98
Gustavo <gcostacoelho2003@gmail.com>
2025-04-08 08:40:04 -0300

    fix: fix known_hosts name file

--- vars/infrastructureTest.groovy
+++ vars/infrastructureTest.groovy
@@ -5,33 +5,23 @@ def call(body) {
     body.delegate = settings
     body()
 
-    container("infra-test") {
-        
+    container("infra-test") {        
         // Install stage dependencies
         sh "apk add openssh curl"
 
-
-        // Prepare the ssh environment
         sh '''
             GITEA_HOST="gitea.localhost.com"
+            GITEA_ORGANIZATION_HOST="git@gitea.localhost.com:gustavocosta.me"
 
             mkdir $HOME/.ssh
 
             cp $JENKINS_GITEA_PRIVATE_KEY $HOME/.ssh/id_rsa
             chmod 400 $HOME/.ssh/id_rsa
 
-            cat $HOME/.ssh/id_rsa
-
-            ssh-keyscan ${GITEA_HOST} > $HOME/.ssh/know_hosts
-
-            cat $HOME/.ssh/know_hosts
-        '''
+            ssh-keyscan ${GITEA_HOST} > $HOME/.ssh/known_hosts
         
-        // Clone helm-applications repository
-        sh '''
-            GITEA_ORGANIZATION_HOST="git@gitea.localhost.com:gustavocosta.me"
-
             git clone $GITEA_ORGANIZATION_HOST/helm-applications.git
+        
             ls -l  helm-applications/
         '''
     }
