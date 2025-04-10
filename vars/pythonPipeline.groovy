e10888a44edc589b062025fb9d396eaaec0163e5
Gustavo <gcostacoelho2003@gmail.com>
2025-04-10 18:14:41 -0300

    fix: post indentation

--- vars/pythonPipeline.groovy
+++ vars/pythonPipeline.groovy
@@ -104,15 +104,15 @@ def call(body){
                     }
                 }
             }
-            post {
-                always {
-                    container('helm') {
-                        sh '''
-                            REPOSITORY=${JOB_NAME%/*}
+        }
+        post {
+            always {
+                container('helm') {
+                    sh '''
+                        REPOSITORY=${JOB_NAME%/*}
 
-                            helm delete -n citest ${REPOSITORY}-ci
+                        helm delete -n citest ${REPOSITORY}-ci
                         '''
-                    }
                 }
             }
         }
